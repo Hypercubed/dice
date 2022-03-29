3ecba4017b5d3339b7c399e615fd229c6842da02
J. Harshbarger <hypercubed@gmail.com>
2022-03-28 23:12:54 -0600

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,8 @@ import { AppComponent } from './app.component';
 import { EncodeComponent } from './encode/encode.component';
 import { DecodeComponent } from './decode/decode.component';
 import { InstructionsComponent } from './instructions/instructions.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -50,6 +52,12 @@ import { InstructionsComponent } from './instructions/instructions.component';
     ClipboardModule,
     MatCheckboxModule,
     PasswordStrengthMeterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     Location,
