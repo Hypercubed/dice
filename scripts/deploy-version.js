e0e86d7c6635efee630d79b7068f28f971a202b7
J. Harshbarger <hypercubed@gmail.com>
2024-09-08 14:52:54 -0600

    fix deploy

--- scripts/deploy-version.js
+++ scripts/deploy-version.js
@@ -1,4 +1,4 @@
-const angularConfig = require('../angular.json');
+const angularConfig = require('../project.json');
 const packageJson = require('../package.json');
 const semverParse = require('semver/functions/parse');
 const { run } = require('./gh-pages.js');
@@ -8,7 +8,7 @@ if (!project) {
   project = angularConfig.defaultProject;
 }
 
-const dir = angularConfig.projects[project].architect.build.options.outputPath;
+const dir = angularConfig.targets.build.options.outputPath;
 const dest = `v${semverParse(packageJson.version).major}`;
 
 run({
