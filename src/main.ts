6854a2f9ce76ce89836cf3fb7590ea4738bbc8f0
J. Harshbarger <hypercubed@gmail.com>
2024-09-15 11:59:36 -0600

    lazy load components

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { DecodeComponent } from './app/decode/decode.component';
-import { EncodeComponent } from './app/encode/encode.component';
 import { InstructionsComponent } from './app/instructions/instructions.component';
-import { provideRouter, Routes, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 import { provideZxvbnServiceForPSM } from 'angular-password-strength-meter/zxcvbn';
 import { Location, LocationStrategy, HashLocationStrategy, CommonModule } from '@angular/common';
 
@@ -23,15 +14,15 @@ const routes: Routes = [
   { path: '', component: InstructionsComponent },
   {
     path: 'encode',
-    component: EncodeComponent,
+    loadComponent: () => import('./app/encode/encode.component').then((m) => m.EncodeComponent),
   },
   {
     path: 'decode',
-    component: DecodeComponent,
+    loadComponent: () => import('./app/decode/decode.component').then((m) => m.DecodeComponent),
   },
   {
     path: 'decode/:encoded',
-    component: DecodeComponent,
+    loadComponent: () => import('./app/decode/decode.component').then((m) => m.DecodeComponent),
   },
 ];
 
@@ -44,21 +35,12 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(
       CommonModule,
       BrowserModule,
-      MatToolbarModule,
-      MatIconModule,
-      MatButtonModule,
-      MatCardModule,
-      MatDividerModule,
-      MatExpansionModule,
       ServiceWorkerModule.register('ngsw-worker.js', {
         enabled: environment.production,
         // Register the ServiceWorker as soon as the application is stable
         // or after 30 seconds (whichever comes first).
         registrationStrategy: 'registerWhenStable:30000',
-      }),
-      RouterOutlet,
-      RouterLink,
-      RouterLinkActive
+      })
     ),
     Location,
     { provide: LocationStrategy, useClass: HashLocationStrategy },
